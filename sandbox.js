980bc48709dc3a09b64f50585c852d4e81142e02
Im-Hassan-wd <alhassansahih@gmail.com>
2021-07-08 13:09:37 -0400

    first touches

---
+++ sandbox.js
@@ -0,0 +1,19 @@
+const lowerBtn = document.querySelector('.lower');
+const addBtn = document.querySelector('.add');
+const count = document.querySelector('.counter');
+
+let counter = '';
+
+lowerBtn.addEventListener('click' , () => {
+  if(counter > 0 && counter < 50){
+    counter--
+    count.textContent = counter
+  }
+});
+
+addBtn.addEventListener('click' , () => {
+  if(count.textContent >= 0 && counter <= 49){
+    counter++
+    count.textContent = counter;
+  }
+});
